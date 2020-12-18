4cc03174f228dbf6f0681606f41dc25f279e1d12
gcvarela21 <gvarela2@horizon.csueastbay.edu>
2020-12-18 00:37:05 -0800

    in style js create and comment out the suedo code for the project plan of action. remove debug log. it was causing a warning / error.

--- script.js
+++ script.js
@@ -12,3 +12,23 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//////////////////////////////////////////////////////////
+// SudoCode Below
+//////////////////////////////////////////////////////////
+// 1. Create functions that generate a random Number, Symbol, lowerCase, and upperCase
+// 2. How to get results of fuctions that create the random things
+// 3. Create a prompt and series of Comfirms from generate button click to ask for:
+//     - length (prompt)
+//     -if length is 8> or = 128  then alert ("needs to be more 8 or more characters or no greater than 129")
+//     -length (prompt)
+//     -if length is is within param then confirm characters
+//     -confirm Number
+//     -confirm Symbol
+//     -confirm upperCase
+//     -confirm lowerCase
+//     -check if at least 1 charset is chosen if false then Alert()
+//     3.1 -ruturn results from confirms to see what we need to build password
+//     3.4 build and array/ function to put it together
+// 4. document.print results into generate text textarea!
+// 5. done... super easy
