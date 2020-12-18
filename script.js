435413d95608c6547dae5475bf68dca99277c22b
gcvarela21 <gvarela2@horizon.csueastbay.edu>
2020-12-18 02:23:55 -0800

    create function randomLower to generate lower case letter fromCharCode using Math.floor and Math.random. create console.log to test the return value fo the function.

--- script.js
+++ script.js
@@ -14,9 +14,9 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 //////////////////////////////////////////////////////////
-// SudoCode Below
+// SudoCode Below (The Game Plan)
 //////////////////////////////////////////////////////////
-// 1. Create functions that generate a random Number, Symbol, lowerCase, and upperCase
+// 1. Create functions that generate a random Number, Symbol, lowerCase, and upperCase from charset
 // 2. How to get results of fuctions that create the random things
 // 3. Create a prompt and series of Comfirms from generate button click to ask for:
 //     - length (prompt)
@@ -32,3 +32,21 @@ generateBtn.addEventListener("click", writePassword);
 //     3.4 build and array/ function to put it together
 // 4. document.print results into generate text textarea!
 // 5. done... super easy
+
+////////////////////////////////////////////////////////////
+// Generator code functions
+///////////////////////////////////////////////////////////
+
+//// my own fucntion. Pulling from the ASCII Character Set
+function randomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+///testing math.random to the cosole. math floor for whole numbers
+// Math.random() generates a random decimal less than 1.0
+// Math.random() * 10 generates a random decimal from 0 to a value less than 10
+// * 26 to get something from 0-25. corresponds with amount of letters to the alphabet when counting from zero
+// + 97 because lower case alpha starts at 97 in the char set code
+/// console log example of a value of 97+ that relates to the char set code
+console.log(Math.floor(Math.random() * 26) + 97);
+//console.log the retun value from function randomLower();
+console.log(randomLower());
